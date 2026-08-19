67b0c4dd78f51784432ec1b487a436316b59fd67
mazharulmrf-mrf <mazharul.mrf@gmail.com>
2026-08-19 18:55:47 +0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { setupNotifications } from "./notifications";
+
+setupNotifications();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
