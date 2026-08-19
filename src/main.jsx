ead485163f4674e44779e0121f7c3a4d61322e27
mazharulmrf-mrf <mazharul.mrf@gmail.com>
2026-08-19 21:45:22 +0600

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { setupNotifications } from "./notifications";
-
-setupNotifications();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
