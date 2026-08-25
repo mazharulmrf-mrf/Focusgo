eca8e39d9245c2f98ca272ee24bfd67aaf0f7683
mazharulmrf-mrf <mazharul.mrf@gmail.com>
2026-08-26 05:54:29 +0600

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { GoogleAuth } from "@codetrix-studio/capacitor-google-auth";
-
-// capacitor.config.json এ থাকা GoogleAuth.serverClientId দিয়েই ইনিশিয়ালাইজ হবে
-GoogleAuth.initialize();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
